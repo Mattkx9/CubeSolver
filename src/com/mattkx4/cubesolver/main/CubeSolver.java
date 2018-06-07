99729687b339f7f8bec320f3dd12ab6d052d9a8e
Matthew Mehrtens <mattkx9@gmail.com>
2018-06-06 23:00:45 -0500

    Testing IntelliJ GIT features.

--- src/com/mattkx4/cubesolver/main/CubeSolver.java
+++ src/com/mattkx4/cubesolver/main/CubeSolver.java
@@ -1,6 +1,7 @@
 package com.mattkx4.cubesolver.main;
 
 public class CubeSolver {
+    /* Start Program */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
